e59f7cfad13b39b1b2c0e1f233b2ec3bf5e32123
Benjamin Kealey <benjamin.kealey@gmail.com>
2025-10-22 16:24:45 -0400

    started work. Haven't set up an actual map, but the grid can work to test it logic is correct.

--- Java/Astar/distanceleft.java
+++ Java/Astar/distanceleft.java
@@ -1,7 +1,13 @@
 package Astar;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
 public class distanceleft {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static double distance() {
+        double x = 0;
+        double y = 0;
+        double[] position = {x, y};
+        double distanceleft = sqrt(pow((10 - position[0]), 2) + pow((10 - position[1]), 2));
+        return distanceleft;
     }
 }
--- Java/Astar/principal.java
+++ Java/Astar/principal.java
@@ -1,7 +1,38 @@
 package Astar;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class principal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        Scanner scanner = new Scanner(System.in);
+        int x = -1;
+        int y = -1;
+        String[][] grid ={{"■","■","■","■","■","■","■","■","■","□"},
+                          {"■","□","□","□","□","□","□","□","□","■"},
+                          {"■","□","■","■","■","■","■","■","□","■"},
+                          {"■","□","■","□","□","□","□","■","□","■"},
+                          {"■","□","■","□","■","■","□","■","□","■"},
+                          {"■","□","■","□","■","□","□","□","□","■"},
+                          {"■","■","■","□","■","□","■","■","■","■"},
+                          {"■","□","□","□","□","□","□","□","□","■"},
+                          {"■","□","■","■","■","■","■","■","□","■"},
+                          {"■","■","■","■","□","■","■","■","□","■"}};
+        
+        while (x < 0 || x > grid[0].length - 1 || y < 0 || y > grid.length - 1) {
+            x = -1;
+            y = -1;
+            try {
+                System.out.print("Enter the x coordinate (0-9): ");
+                x = scanner.nextInt();
+                System.out.print("Enter the y coordinate (0-9): ");
+                y = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter numbers only.");
+                scanner.next(); // clear the invalid input
+            }
+        } scanner.close();
+
+
+    };
 }
+
